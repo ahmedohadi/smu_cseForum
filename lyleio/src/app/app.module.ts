3ec360abec1ad07af29f414d526c566b1ec30689
Ahmed Hadi <ahadi@smu.edu>
2018-11-12 15:51:45 -0600

    add routings on angualr

--- lyleio/src/app/app.module.ts
+++ lyleio/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
@@ -9,6 +9,16 @@ import { ChatBoardComponent } from './chat-board/chat-board.component';
 import { CommentsComponent } from './chat-board/comments/comments.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileUpdateComponent } from './profile-update/profile-update.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'signin', component: SigninComponent},
+  { path: 'signup', component: SignupComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'profileUpdate', component: ProfileUpdateComponent},
+  { path: 'logout', component: SigninComponent}
+];
+
 
 @NgModule({
   declarations: [
@@ -22,7 +32,8 @@ import { ProfileUpdateComponent } from './profile-update/profile-update.componen
     ProfileUpdateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
